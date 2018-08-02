281459e487776218207d16165cd2d8966b43cee1
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:14:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Code your solution in this file!
-function view_all_property(obj, key){
-    obj.map( (value) => { console.log(value[key]) } )
-}
 function logDriverNames(a_drivers){
-  view_all_property(a_drivers, "name")
+    a_drivers.map( (value) => { console.log(value[key]) } )
 }
 
 function driversByRevenue(a_drivers){
-    view_all_property(a_drivers, "revenue")
+    view_all_property(a_drivers, "hometown")
 }
