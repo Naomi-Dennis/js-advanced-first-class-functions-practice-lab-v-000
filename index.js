a33bb6585bdd22f98abfe23a7936d1ffa4489518
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function logDriverNames(a_drivers){
 
 function driversByRevenue(a_drivers, hometown){
     for(const driver in a_drivers){
-       (driver["hometown"] == hometown) ? driver["name"] : ""
+       console.log( (driver["hometown"] == hometown) ? driver["name"] : "" )
     }
 }
