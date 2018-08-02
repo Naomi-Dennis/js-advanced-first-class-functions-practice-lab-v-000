31b8fae3be508ec7d61a3918676c21b71f9f7471
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:12:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 
 function logDriverNames(a_drivers){
-    a_drivers.map( (value) => { console.log(a_drivers["name"])s } )
+    a_drivers.map( (value) => { console.log(value["name"]) } )
 }
 
 function driversByRevenue(a_drivers){
