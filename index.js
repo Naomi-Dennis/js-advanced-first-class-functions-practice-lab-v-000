aa288df27395a5409a516e27c7cea1189f540748
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 15:13:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code your solution in this file!
-
+function view_all_property(obj, key){
+    obj.map( (value) => { console.log(value[key]) } )
+}
 function logDriverNames(a_drivers){
-    a_drivers.map( (value) => { console.log(value["name"]) } )
+  view_all_property(a_drivers, "name")
 }
 
 function driversByRevenue(a_drivers){
